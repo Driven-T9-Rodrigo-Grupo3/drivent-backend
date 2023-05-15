511213cecd7478e58fa3318a0d7fff0d5f3f3c3a
Ranbut <Ranbut_Beater@protonmail.com>
2023-05-15 14:23:28 -0300

    feat: bookingByRoomId

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { bookingRoom, changeBooking, listBooking } from '@/controllers';
+import { bookingRoom, changeBooking, listBooking, listBookingByRoom } from '@/controllers';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('', listBooking).post('', bookingRoom).put('/:bookingId', changeBooking);
+bookingRouter
+  .all('/*', authenticateToken)
+  .get('', listBooking)
+  .get('/:roomId', listBookingByRoom)
+  .post('', bookingRoom)
+  .put('/:bookingId', changeBooking);
 
 export { bookingRouter };
